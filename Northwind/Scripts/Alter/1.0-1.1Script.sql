cb4dc2cd0331bf50e5f1203805372c66b1cab6d4
lizasixsixsix <lizasixsixsix@gmail.com>
2018-05-01 23:53:35 +0300

    scripts 1.0-1.1 added

---
+++ Northwind/Scripts/Alter/1.0-1.1Script.sql
@@ -0,0 +1,23 @@
+﻿if object_id('EmployeeCreditCards') is null
+
+begin
+    create table [Northwind].[EmployeeCreditCards]
+    (
+        [CardID]            [int]             identity(1,1)    not null,
+        [CardNumber]        [varchar](20)                      not null,
+        [CardHolderName]    [nvarchar](50)                     not null,
+        [ExpirationDate]    [datetime]                         not null,
+        [EmployeeID]        [int]                              not null,
+
+        constraint [PK_EmployeeCreditCards] primary key clustered
+        (
+            [CardNumber] asc
+        )
+    );
+
+    alter table [Northwind].[EmployeeCreditCards]
+        with nocheck
+        add constraint [FK_EmployeeCreditCards_Employees]
+        foreign key ([EmployeeID])
+        references [Northwind].[Employees] ([EmployeeID]);
+end
